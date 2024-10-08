12b74d375c5a7b829f9e89d6d781cb593d4efcb7
vleksyyy <veloso.julia@escola.pr.gov.br>
2024-10-08 08:23:22 -0300

    aaa

---
+++ perguntas.js
@@ -0,0 +1,107 @@
+criaCartao (
+    'Física',
+    'Como funciona os cometas',
+    'Os cometas realizam órbita elíptica em torno do Sol, e a sua duração pode ser de algumas centenas até milhares de anos. Uma parte do trajeto dos cometas é realizada mais próximo dessa estrela, o que provoca a sublimação dos gases presentes em sua estrutura.' )
+    
+    criaCartao (
+    'Quimica',
+    'O que é quimica organica ',
+    'A química orgânica é o campo da química dedicado ao estudo dos compostos de carbono, também conhecidos como compostos orgânicos.' )
+    
+    
+    criaCartao (
+    'Lingua Portuguesa',
+    'Quando foi a segunda fase do modernismo',
+    'Foi no periodo de 1930 a 1945.' )
+    
+    
+    criaCartao (
+    'Matematica',
+    'Como calcular log',
+    'Uma das maneiras é procurar um número que, quando elevamos a base, resulte no logaritmando .' )
+    
+    criaCartao (
+    'Biologia',
+    'O que é impulso nervoso',
+    'É um sinal elétrico que permite a comunicação entre neurônios.' )
+    
+    criaCartao (
+    'Matematica',
+    'Como calcula a soma de pg',
+    'Para calcular a soma dos termos de uma progressão geométrica, é possível utilizar a fórmula Sn = a1(qn-1)/q-1.' )
+    
+    
+    criaCartao (
+    'Física',
+    'Como funciona a energia eolica',
+    'É gerada a partir da força do vento, que é transformada em energia elétrica por meio de aerogeradores.' )
+    
+    criaCartao (
+    'Biologia ',
+    'Como funciona o cérebro',
+    'Ele processa informações e gera pensamentos e ideias, além de enviar mensagens para o corpo.' )
+    
+    criaCartao (
+    'Quimica',
+    'Como calcular a massa molar',
+    'Dividindo a massa (m) da matéria por sua massa molar (M). ' )
+    
+    
+    criaCartao (
+    'Quimica',
+    'O que é quimica organica',
+    'É a parte da Química que estuda os compostos do elemento carbono com propriedades características.' )
+    
+    criaCartao (
+    'Lingua Portuguesa',
+    'Como foi a primeira fase do modernismo',
+    'Trouxe para a literatura a inovação e fez uma releitura dos símbolos nacionais.' )
+    
+    criaCartao (
+    'Matemática',
+    'Quais são a propriedades da geometria plana',
+    'Geometria plana é a área da matemática que estuda as figuras geométricas bidimensionais, que não possuem volume, como triângulos, retângulos, círculos.' )
+    
+    criaCartao (
+    'Quimica',
+    'O que é quimica inorgânica',
+    'A Química Inorgânica estuda as substâncias que apresentam como origem principal o reino mineral.' )
+    
+    criaCartao (
+    'Biologia',
+    'Quais são os orgãos do sistema urinário',
+    'Os órgãos do sistema urinário são: Dois rins, Dois ureteres, Uma bexiga urinária, Uma uretra.' )
+    
+    criaCartao (
+    'Biologia ',
+    'Quais sao os efeitos das drogas sintéticas ao longo do tempo',
+    'Os riscos das drogas sintéticas são variados, incluindo-se danos cerebrais, convulsões, surtos psicóticos e até mesmo a morte.' )
+    
+    
+    
+    criaCartao (
+    'Educação Fisica',
+    'O que fazer quando uma pessoa desmaiar',
+    'Afastar a vítima de local que proporcione perigo. Deitá-la de barriga para cima, e elevar as pernas acima do tórax (com a cabeça mais baixa em relação ao restante do corpo). Lateralizar a cabeça para facilitar a respiração. Afrouxar as roupas.' )
+    
+    criaCartao (
+    'Física',
+    'quais sao os beneficios da energia hidrelétrica',
+    'A hidreletricidade contribui para o armazenamento de água potável. Os reservatórios das usinas hidrelétricas coletam a água da chuva, que pode então ser usada para consumo ou para irrigação.' )
+    
+    
+    criaCartao (
+    'Matematica',
+    'O que é trigonometria',
+    'A trigonometria é uma área da geometria plana euclidiana que analisa a relação existente entre os ângulos de um triângulo e o comprimento dos seus lados.' )
+    
+    criaCartao (
+    'Quimica',
+    'Como o petróleo foi descoberto',
+    'Em 1859, um americano chamado Edwin Drake perfurou na Pensilvânia um poço à procura de petróleo, que possuía grande quantidade para extração. Esse poço foi descoberto em uma região de pouca profundidade, de aproximadamente 21 metros, ao contrário das perfurações de hoje, que superam os 6.000 metros.' )
+    
+    
+    criaCartao (
+    'Lingua Portuguesa',
+    'Quais são as características da primeira fase do modernismo',
+    'Primeira geração modernista (1922-1930) Antiacademicismo, Liberdade formal, Ironia, Aproximação entre fala e escrita.' )
